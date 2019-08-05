061707673a5de361000b020e7c14ce6f6a9e037a
Dominik Tugend <dominik@matrixstorm.com>
2019-08-05 21:58:04 +0200

    Fix installer language

---
+++ installer/BAFunctions/bafunctions.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved. Licensed under the Microsoft Reciprocal License. See LICENSE.TXT file in the project root for full license information.
+
+#include "precomp.h"
+
+static HINSTANCE vhInstance = NULL;
+
+extern "C" BOOL WINAPI DllMain(
+	IN HINSTANCE hInstance,
+	IN DWORD dwReason,
+	IN LPVOID /* pvReserved */
+)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		::DisableThreadLibraryCalls(hInstance);
+		vhInstance = hInstance;
+		break;
+
+	case DLL_PROCESS_DETACH:
+		vhInstance = NULL;
+		break;
+	}
+
+	return TRUE;
+}
