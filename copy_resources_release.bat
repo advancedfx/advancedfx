6eca310e147b4e8a7b7a829ab56973313b2350b1
Dominik Tugend <dominik@matrixstorm.com>
2021-11-30 19:25:48 +0100

    Fix #699 cmake install step fails with space character in HLAE source path

--- copy_resources_release.bat
+++ copy_resources_release.bat
@@ -1,7 +1,7 @@
-xcopy "resources\*" "%1" /D /E /Y
-xcopy "deps\release\Detours\LICENSE.md" "%1\LICENSES\Detours\" /D /Y
-xcopy "deps\release\rapidxml\license.txt" "%1\LICENSES\rapidxml\" /D /Y
-xcopy "deps/release/easywsclient\COPYING" "%1\LICENSES\easywsclient\" /D /Y
-xcopy "LICENSE" "%1\LICENSES\advancedfx\" /D /Y
-xcopy "shaders\build\*.acs" "%1\resources\AfxHookSource\shaders\" /D /Y
-powershell .\make_readme.ps1 "%1"
+xcopy "resources\*" "%~1" /D /E /Y
+xcopy "deps\release\Detours\LICENSE.md" "%~1\LICENSES\Detours\" /D /Y
+xcopy "deps\release\rapidxml\license.txt" "%~1\LICENSES\rapidxml\" /D /Y
+xcopy "deps/release/easywsclient\COPYING" "%~1\LICENSES\easywsclient\" /D /Y
+xcopy "LICENSE" "%~1\LICENSES\advancedfx\" /D /Y
+xcopy "shaders\build\*.acs" "%~1\resources\AfxHookSource\shaders\" /D /Y
+powershell .\make_readme.ps1 "%~1"
