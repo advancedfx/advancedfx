1f95ef5646d4a6d6c5a7a4f5cce174fa096d2a02
Dominik Tugend <dominik@matrixstorm.com>
2022-02-02 14:06:48 +0100

    HLAE 2.125.1

--- hlae/UpdateCheck.cs
+++ hlae/UpdateCheck.cs
@@ -51,11 +51,11 @@ namespace AfxGui
 
             m_Guids = new Guid[]{
                 // current GUID:
-                new Guid("e881c735-8490-47c6-a08f-82b71c1f1e2f"),
+                new Guid("149db9e0-c3a3-4b19-ac37-c853298b4368"),
                 // current roll-back GUID:
-                new Guid("64cc0784-49c4-4cda-a0fd-781993cefba0"),
+                new Guid("2b4f448b-df25-4366-951a-9c399c063bf0"),
                 // old GUID(s) to accept:
-                new Guid("97cb5920-c474-42e1-b93f-fdda1ecd1ae0")
+                new Guid("e881c735-8490-47c6-a08f-82b71c1f1e2f")
             };
 
 	        m_Targets = new LinkedList<UpdateCheckNotificationTarget>();
