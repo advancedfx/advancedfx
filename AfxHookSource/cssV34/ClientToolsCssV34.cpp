a33a5b929546ee9a3c31a3e9d77a2f9d9c8384a6
Dominik Tugend <dominik@matrixstorm.com>
2022-04-14 16:29:16 +0200

    Fix CSSV34 AGRv6 RecordBones hook 4th time

--- AfxHookSource/cssV34/ClientToolsCssV34.cpp
+++ AfxHookSource/cssV34/ClientToolsCssV34.cpp
@@ -22,7 +22,7 @@ std::vector<SOURCESDK::matrix3x4_t> g_cssv34_BoneState;
 typedef void *  (__fastcall * cssv34_C_BaseAnimating_RecordBones_t)(void* This, void* Edx, SOURCESDK::CStudioHdr *hdr);
 cssv34_C_BaseAnimating_RecordBones_t True_cssv34_C_BaseAnimating_RecordBones = nullptr;
 void * __fastcall My_cssv34_C_BaseAnimating_RecordBones(void* This, void* Edx, SOURCESDK::CStudioHdr *hdr) {
-	void * result = True_cssv34_C_BaseAnimating_RecordBones(hdr, Edx, hdr);
+	void * result = True_cssv34_C_BaseAnimating_RecordBones(This, Edx, hdr);
 	g_cssv34_hdr =  hdr;
 	SOURCESDK::matrix3x4_t *pBoneState = *(SOURCESDK::matrix3x4_t **)((unsigned char *)This + AFXADDR_GET(cssv34_client_C_BaseAnimating_m_BoneAccessor_m_pBones));
 	if(g_cssv34_BoneState.size() < hdr->numbones()) g_cssv34_BoneState.resize(hdr->numbones());
