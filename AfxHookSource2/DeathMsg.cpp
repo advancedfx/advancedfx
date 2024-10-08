advancedfx/advancedfx

commit 038853c909c858a5dda96e54abbcc5d55343e6e2

Fix bug with local player in `mirv_deathmsg` in AfxHookSource2.

AfxHookSource2/DeathMsg.cpp
@@ -134,6 +134,7 @@ PlayerInfo getPlayerInfoFromControllerIndex(int entindex)
 	{
         if(auto ent = (CEntityInstance*)g_GetEntityFromIndex(*g_pEntityList,i))
 		{
+			if(i != entindex) continue;
 			if(!ent->IsPlayerController()) continue;
 
 			auto teamNumber = *(int*)((u_char*)(ent) + g_clientDllOffsets.C_BaseEntity.m_iTeamNum);
@@ -657,18 +658,26 @@ uint64_t __fastcall getLocalSteamId(void* param_1) {
 	bool use = false;
 
 	if (nullptr != g_MirvDeathMsgGlobals.activeWrapper) { 
-		if (g_MirvDeathMsgGlobals.activeWrapper->attacker.isLocal.use && g_MirvDeathMsgGlobals.activeWrapper->attacker.isLocal.value) {
+		if (g_MirvDeathMsgGlobals.activeWrapper->attacker.isLocal.use) {
 			entry = g_MirvDeathMsgGlobals.activeWrapper->attacker;			
 			use = true;
 		}
-		if (g_MirvDeathMsgGlobals.activeWrapper->victim.isLocal.use && g_MirvDeathMsgGlobals.activeWrapper->victim.isLocal.value) {
+		if (g_MirvDeathMsgGlobals.activeWrapper->victim.isLocal.use) {
 			entry = g_MirvDeathMsgGlobals.activeWrapper->victim;
 			use = true;
 		}
-		if (g_MirvDeathMsgGlobals.activeWrapper->assister.isLocal.use && g_MirvDeathMsgGlobals.activeWrapper->assister.isLocal.value) {
+		if (g_MirvDeathMsgGlobals.activeWrapper->assister.isLocal.use) {
 			entry = g_MirvDeathMsgGlobals.activeWrapper->assister;
 			use = true;
 		}
+	}
+
+	if (use && !entry.isLocal.value) return 0;
+
+	if (g_MirvDeathMsgGlobals.useHighlightId)
+	{
+		entry.newId.value = g_MirvDeathMsgGlobals.highlightId;
+		use = true;
 	}
 
 	if (!use) return g_Original_getLocalSteamId(param_1);
@@ -706,9 +715,7 @@ uint64_t __fastcall getLocalSteamId(void* param_1) {
 		break;
 	};
 
-	if (0 != result) return result;
-
-	return g_Original_getLocalSteamId(param_1);
+	return result;
 };
 
 // param 1 CCSGO_HudDeathNotice : panorama::CPanel2D : panorama::IUIPanelClient : CCSGOHudElement : CGameEventListener : IGameEventListener2

commit 59b8828dc82f1720fc6fd7dfbe11c4d5c6bec7c6

Fix again `mirv_deathmsg` in AfxHookSource2.
Fix for two bugs introduced in https://github.com/advancedfx/advancedfx/commit/038853c909c858a5dda96e54abbcc5d55343e6e2
- fix another bug with localplayer.
- fix wrong speckeys.

AfxHookSource2/DeathMsg.cpp
@@ -134,11 +134,27 @@ PlayerInfo getPlayerInfoFromControllerIndex(int entindex)
 	{
         if(auto ent = (CEntityInstance*)g_GetEntityFromIndex(*g_pEntityList,i))
 		{
-			if(i != entindex) continue;
 			if(!ent->IsPlayerController()) continue;
 
 			auto teamNumber = *(int*)((u_char*)(ent) + g_clientDllOffsets.C_BaseEntity.m_iTeamNum);
 			if (0 == teamNumber || 1 == teamNumber) continue;
+
+			int slot = 0;
+			if (3 == teamNumber) // CT
+			{
+				slot = 1 + slotCT;
+				if (swapPlayerSide) slot += 5;
+				++slotCT;
+			} 
+			else if (2 == teamNumber) // T
+			{
+				slot = 1 + slotT;
+				if (!swapPlayerSide) slot += 5;
+				++slotT;
+			}
+			slot = slot % 10;
+
+			if(i != entindex) continue;
 
 			auto xuid = *(uint64_t*)((u_char*)(ent) + g_clientDllOffsets.CBasePlayerController.m_steamID);
 			auto name = (char*)((u_char*)(ent) + g_clientDllOffsets.CBasePlayerController.m_iszPlayerName);
@@ -150,22 +166,6 @@ PlayerInfo getPlayerInfoFromControllerIndex(int entindex)
 			result.xuid = xuid;
 			result.playerController = (u_char*)(ent);
 			result.userId = i - 1;
-
-			int slot = 0;
-			if (3 == teamNumber) // CT
-			{
-				slot = 1 + slotCT;
-				if (swapPlayerSide) slot += 5;
-				++slotCT;
-			} else
-			if (2 == teamNumber) // T
-			{
-				slot = 1 + slotT;
-				if (!swapPlayerSide) slot += 5;
-				++slotT;
-			}
-			slot = slot % 10;
-
 			result.specKey = slot;
 
 			if (i == entindex) break;
@@ -662,23 +662,24 @@ uint64_t __fastcall getLocalSteamId(void* param_1) {
 			entry = g_MirvDeathMsgGlobals.activeWrapper->attacker;			
 			use = true;
 		}
-		if (g_MirvDeathMsgGlobals.activeWrapper->victim.isLocal.use) {
+		else if (g_MirvDeathMsgGlobals.activeWrapper->victim.isLocal.use) {
 			entry = g_MirvDeathMsgGlobals.activeWrapper->victim;
 			use = true;
 		}
-		if (g_MirvDeathMsgGlobals.activeWrapper->assister.isLocal.use) {
+		else if (g_MirvDeathMsgGlobals.activeWrapper->assister.isLocal.use) {
 			entry = g_MirvDeathMsgGlobals.activeWrapper->assister;
 			use = true;
 		}
 	}
 
-	if (use && !entry.isLocal.value) return 0;
-
 	if (g_MirvDeathMsgGlobals.useHighlightId)
 	{
 		entry.newId.value = g_MirvDeathMsgGlobals.highlightId;
+		entry.isLocal.value = true;
 		use = true;
 	}
+
+	if (use && !entry.isLocal.value) return 0;
 
 	if (!use) return g_Original_getLocalSteamId(param_1);
 