8c9ca5142d47509aa1024ed123c9fba41ff453c4
ChetdeJong <floweplay@gmail.com>
2024-11-03 13:11:53 +0400

    Revert "mirv-script: expose campath iterator"
    This reverts commit c6f0a46d0f6a763a28663d58f40a6c0ab447a7a8.

--- misc/mirv-script/src/types/campath.d.ts
+++ misc/mirv-script/src/types/campath.d.ts
@@ -82,22 +82,6 @@ declare class AdvancedfxCampath {
 	readonly size: number;
 
 	/**
-	 * Iterator.
-	 * @remarks
-	 * Used to iterate over all keyframes in campath.
-	 *
-	 * Example:
-	 * ```ts
-	 * while (iterator.valid) {
-	 *		const campathPointValue = iterator.value;
-	 *		const campathPointTime = iterator.time;
-	 *		iterator.next();
-	 * }
-	 * ```
-	 */
-	readonly iterator: AdvancedfxCampathIterator;
-
-	/**
 	 * Length of path in seconds.
 	 */
 	readonly duration: number;
