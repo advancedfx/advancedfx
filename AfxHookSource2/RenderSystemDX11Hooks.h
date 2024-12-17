1891884d7a737f46566183a89b6285ced1345642
Dec <40070135+Dechno1337@users.noreply.github.com>
2024-12-17 20:21:14 +0100

    Typos
    Will adjust the other instances where it's wrong.

--- AfxHookSource2/RenderSystemDX11Hooks.h
+++ AfxHookSource2/RenderSystemDX11Hooks.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define ADVNACEDFX_STARTMOIVE_WAV_KEY "advancedfx-802bb089-972b-4841-bdf3-5108175ab59d"
+#define ADVANCEDFX_STARTMOVIE_WAV_KEY "advancedfx-802bb089-972b-4841-bdf3-5108175ab59d"
 
 bool AfxStreams_IsRcording();
 const wchar_t * AfxStreams_GetTakeDir();
