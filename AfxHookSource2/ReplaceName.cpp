a3109ff9c6d330a917aab69d6a7d672456be0268
Dominik Tugend <dominik@matrixstorm.com>
2025-06-19 22:28:40 +0200

    Improve mirv_replace name
    Will not replace chat messages, we recommend tv_nochat false as workaround

--- AfxHookSource2/ReplaceName.cpp
+++ AfxHookSource2/ReplaceName.cpp
@@ -22,27 +22,38 @@
 #include <string>
 #include <map>
 
+
+bool g_bDebug_GetPlayerName = false;
+bool g_bDebug_GetDecoratedPlayerName = false;
+
 std::map<int,std::string> g_Index_To_ReplaceName;
 std::map<uint64_t,std::string> g_SteamId_To_ReplaceName;
 
+std::map<int,std::string> g_Index_To_DecoratedReplaceName;
+std::map<uint64_t,std::string> g_SteamId_To_DecoratedReplaceName;
+
+
 typedef SOURCESDK::CS2::CEntityHandle * (__fastcall * CEntitityIntance_GetRefEHandle_t)(void * This, SOURCESDK::CS2::CEntityHandle & handle);
 
-typedef void (__fastcall * CEntitityIntance_PostDataUpdate_t)(void * This);
-CEntitityIntance_PostDataUpdate_t g_pOrg_CCSPlayerController_PostDataUpdate = nullptr;
+typedef const char * (__fastcall * CCSPlayerController_GetPlayerName_t)(void * This);
+CCSPlayerController_GetPlayerName_t g_Org_CCSPlayerController_GetPlayerName = nullptr;
 
-void __fastcall New_CCSPlayerController_PostDataUpdate(void * This) {
-    g_pOrg_CCSPlayerController_PostDataUpdate(This);
+const char * __fastcall New_CCSPlayerController_GetPlayerName(void * This) {
+    const char * result = g_Org_CCSPlayerController_GetPlayerName(This);
+
+    if(g_bDebug_GetPlayerName) {
+        if(void * pEntityIndentity = *(void**)((unsigned char*)This + g_clientDllOffsets.CEntityInstance.m_pEntity)) {
+            SOURCESDK::CS2::CEntityHandle * pHandle = (SOURCESDK::CS2::CEntityHandle *)((unsigned char *)pEntityIndentity + 0x10);
+            advancedfx::Message("GetPlayerName: %i -> %s\n",pHandle->GetEntryIndex(),result);
+        }        
+    }
 
     if(!g_Index_To_ReplaceName.empty()){
         if(void * pEntityIndentity = *(void**)((unsigned char*)This + g_clientDllOffsets.CEntityInstance.m_pEntity)) {
             SOURCESDK::CS2::CEntityHandle * pHandle = (SOURCESDK::CS2::CEntityHandle *)((unsigned char *)pEntityIndentity + 0x10);
             auto it = g_Index_To_ReplaceName.find(pHandle->GetEntryIndex());
             if(it != g_Index_To_ReplaceName.end()) {
-                char * pszTarget = (char * )((unsigned char*)This + g_clientDllOffsets.CBasePlayerController.m_iszPlayerName);
-                strncpy(pszTarget,it->second.c_str(),127);
-                pszTarget[128] = '\0';
-
-                ((SOURCESDK::CS2::CUtlString *)((unsigned char*)This + g_clientDllOffsets.CCSPlayerController.m_sSanitizedPlayerName))->Set(it->second.c_str());
+                result = it->second.c_str();
             }
         }
     }
@@ -51,33 +62,92 @@ void __fastcall New_CCSPlayerController_PostDataUpdate(void * This) {
         uint64_t steamid = *(uint64_t*)((unsigned char*)This + g_clientDllOffsets.CBasePlayerController.m_steamID);
         auto it = g_SteamId_To_ReplaceName.find(steamid);
         if(it!=g_SteamId_To_ReplaceName.end()) {
-            char * pszTarget = (char * )((unsigned char*)This + g_clientDllOffsets.CBasePlayerController.m_iszPlayerName);
-            strncpy(pszTarget,it->second.c_str(),127);
-            pszTarget[128] = '\0';
-
-            ((SOURCESDK::CS2::CUtlString *)((unsigned char*)This + g_clientDllOffsets.CCSPlayerController.m_sSanitizedPlayerName))->Set(it->second.c_str());
+                result = it->second.c_str();
         }
     }
+
+    return result;
 }
 
-bool HookReplaceName(HMODULE clientDll)
+typedef const char * (__fastcall * GetDecoratedPlayerName_t)(void *This_CCSPlayerController, char * pBuffer , unsigned int bufferSize, unsigned int maybeShortenLength);
+GetDecoratedPlayerName_t g_Org_GetDecoratedPlayerName = nullptr;
+const char * __fastcall New_GetDecoratedPlayerName(void *This_CCSPlayerController, char * pBuffer , unsigned int bufferSize, unsigned int maybeShortenLength) {
+    const char * result = g_Org_GetDecoratedPlayerName(This_CCSPlayerController, pBuffer, bufferSize, maybeShortenLength);
+
+    if(g_bDebug_GetDecoratedPlayerName) {
+        if(void * pEntityIndentity = *(void**)((unsigned char*)This_CCSPlayerController + g_clientDllOffsets.CEntityInstance.m_pEntity)) {
+            SOURCESDK::CS2::CEntityHandle * pHandle = (SOURCESDK::CS2::CEntityHandle *)((unsigned char *)pEntityIndentity + 0x10);
+            advancedfx::Message("GetDecoratedPlayerName: %i -> %s\n",pHandle->GetEntryIndex(),result);
+        }        
+    }    
+
+    if(!g_Index_To_DecoratedReplaceName.empty()){
+        if(void * pEntityIndentity = *(void**)((unsigned char*)This_CCSPlayerController + g_clientDllOffsets.CEntityInstance.m_pEntity)) {
+            SOURCESDK::CS2::CEntityHandle * pHandle = (SOURCESDK::CS2::CEntityHandle *)((unsigned char *)pEntityIndentity + 0x10);
+            auto it = g_Index_To_DecoratedReplaceName.find(pHandle->GetEntryIndex());
+            if(it != g_Index_To_DecoratedReplaceName.end()) {
+                strncpy(pBuffer,it->second.c_str(),bufferSize);
+            }
+        }
+    }
+
+    if(!g_SteamId_To_DecoratedReplaceName.empty()) {
+        uint64_t steamid = *(uint64_t*)((unsigned char*)This_CCSPlayerController + g_clientDllOffsets.CBasePlayerController.m_steamID);
+        auto it = g_SteamId_To_DecoratedReplaceName.find(steamid);
+        if(it!=g_SteamId_To_DecoratedReplaceName.end()) {
+           strncpy(pBuffer,it->second.c_str(),bufferSize);
+        }
+    }
+
+    return result;
+}
+
+void HookReplaceName(HMODULE clientDll)
 {
     static bool firstRun = true;
-    static bool firstResult = false;
     if(firstRun) {
         firstRun = false;
+
+        // GetDecoratedPlayerName
+        // references "SFUI_bot_decorated_name"       
+	    {
+            Afx::BinUtils::ImageSectionsReader sections((HMODULE)clientDll);
+            Afx::BinUtils::MemRange textRange = sections.GetMemRange();
+            Afx::BinUtils::MemRange result = FindPatternString(textRange, "44 89 44 24 18 48 89 54 24 10 55 53 56 57 41 54 41 55 41 56 41 57 48 8d ac 24 28 f5 ff ff");
+            if (!result.IsEmpty()) {
+                g_Org_GetDecoratedPlayerName = (GetDecoratedPlayerName_t)result.Start;	
+                DetourTransactionBegin();
+                DetourUpdateThread(GetCurrentThread());
+                DetourAttach(&(PVOID&)g_Org_GetDecoratedPlayerName, New_GetDecoratedPlayerName);
+                if(NO_ERROR != DetourTransactionCommit()) ErrorBox(MkErrStr(__FILE__, __LINE__));
+            }
+            else ErrorBox(MkErrStr(__FILE__, __LINE__));
+	    }
+
+        /*
+            GetDecoratedPlayerName references a function on "?AVCCSPlayerController@@" vtable as follows:
+            ...
+            puVar15 = &DAT_18120c000;
+            if (plVar14 == (longlong *)0x0) {
+                local_bd8 = "";
+            }
+            else {
+                local_bd8 = (char *)(**(code **)(*plVar14 + 0x6f0))();
+            }
+            ...
+            So now know the offset of the GetPlayerNameFunction
+        */
+
+
         if(void ** vtable = (void **)Afx::BinUtils::FindClassVtable(clientDll, ".?AVCCSPlayerController@@", 0, 0)) {
-            const size_t vtable_ofs = 8;
-            MdtMemBlockInfos mbis;
-            MdtMemAccessBegin((LPVOID)&(vtable[vtable_ofs]), sizeof(void*), &mbis);
-            g_pOrg_CCSPlayerController_PostDataUpdate = (CEntitityIntance_PostDataUpdate_t)vtable[8];
-            vtable[8] = &New_CCSPlayerController_PostDataUpdate;
-			MdtMemAccessEnd(&mbis);
-            firstResult = true;
+            g_Org_CCSPlayerController_GetPlayerName = (CCSPlayerController_GetPlayerName_t)vtable[222];
+            DetourTransactionBegin();
+            DetourUpdateThread(GetCurrentThread());
+            DetourAttach(&(PVOID&)g_Org_CCSPlayerController_GetPlayerName, New_CCSPlayerController_GetPlayerName);
+            if(NO_ERROR != DetourTransactionCommit()) ErrorBox(MkErrStr(__FILE__, __LINE__));
         }
-        else ErrorBox("HookReplaceName");
+        else ErrorBox(MkErrStr(__FILE__, __LINE__));
     }
-    return firstResult;
 }
 
 extern void deathMsgPlayers_PrintHelp_Console();
@@ -156,12 +226,107 @@ CON_COMMAND(mirv_replace_name, "Replace player names")
                 }
             }
         }
+        else if(0 == stricmp("decoByUserId", arg1)) {
+            if(3 <= argC) {
+                const char * arg2 = args->ArgV(2);
+                if(0 == stricmp("add", arg2) && 5 <= argC) {
+                    g_Index_To_DecoratedReplaceName[atoi(args->ArgV(3))+1] = args->ArgV(4);
+                    return;
+                }
+                else if(0 == stricmp("remove", arg2) && 4 <= argC) {
+                    g_Index_To_DecoratedReplaceName.erase(atoi(args->ArgV(3))+1);
+                    return;
+                }
+                else if(0 == stricmp("print", arg2) && 3 <= argC) {
+                    for(auto it = g_Index_To_DecoratedReplaceName.begin(); it != g_Index_To_DecoratedReplaceName.end(); it++) {
+                        advancedfx::Message("%i: %s\n",it->first-1,it->second.c_str());
+                    }
+                    return;
+                }                
+            }
+            advancedfx::Message(
+                "%s decoByUserId add <iUserId> <sValue>\n"
+                "%s decoByUserId remove <iUserId>\n"
+                "%s decoByUserId print\n",
+                arg0,
+                arg0,
+                arg0
+            );
+            return;
+        } else if(0 == stricmp("decoByXuid", arg1)) {
+            if(3 <= argC) {
+                const char * arg2 = args->ArgV(2);
+                if(0 == stricmp("add", arg2) && 5 <= argC) {
+                    const char * arg3 = args->ArgV(3);
+                    if(StringIBeginsWith(arg3,"x")) arg3++;
+                    g_SteamId_To_DecoratedReplaceName[strtoull(arg3,nullptr,10)] = args->ArgV(4);
+                    return;
+                }
+                else if(0 == stricmp("remove", arg2) && 4 <= argC) {
+                    const char * arg3 = args->ArgV(3);
+                    if(StringIBeginsWith(arg3,"x")) arg3++;                    
+                    g_SteamId_To_DecoratedReplaceName.erase(strtoull(arg3,nullptr,10));
+                    return;
+                }
+                else if(0 == stricmp("print", arg2) && 3 <= argC) {
+                    for(auto it = g_SteamId_To_DecoratedReplaceName.begin(); it != g_SteamId_To_DecoratedReplaceName.end(); it++) {
+                        advancedfx::Message("x%llu: %s\n",it->first,it->second.c_str());
+                    }
+                    return;
+                }                
+            }
+            advancedfx::Message(
+                "%s decoByXuid add x<ullXuid> <sValue>\n"
+                "%s decoByXuid remove x<ullXuid>\n"
+                "%s decoByXuid print\n",
+                arg0,
+                arg0,
+                arg0
+            );
+            return;
+        }
+        else if(0==stricmp("debug",arg1)) {
+            if(3 <= argC) {
+                g_bDebug_GetPlayerName = 0 != atoi(args->ArgV(2));
+                return;
+            }
+
+            advancedfx::Message(
+                "%s debug 0|1\n"
+                "Current Value: %i\n",
+                arg0,
+                g_bDebug_GetPlayerName?1:0
+            );
+            return;
+        }
+        else if(0==stricmp("decoDebug",arg1)) {
+            if(3 <= argC) {
+                g_bDebug_GetDecoratedPlayerName = 0 != atoi(args->ArgV(2));
+                return;
+            }
+
+            advancedfx::Message(
+                "%s decoDebug 0|1\n"
+                "Current Value: %i\n",
+                arg0,
+                g_bDebug_GetDecoratedPlayerName?1:0
+            );
+            return;
+        }
     }
 
     advancedfx::Message(
         "%s byUserId [...] - Replace player name by UserID\n"
         "%s byXuid [...] - Replace player name by SteamID\n"
-        "%s help players - Print player info in console\n",
+        "%s help players - Print player info in console\n"
+        "%s decoByUserId [...] - Replace decorated player name by UserID\n"
+        "%s decoByXuid [...] - Replace decorated player name by SteamID\n"
+        "%s debug 0|1\n"
+        "%s decoDebug 0|1\n",
+        arg0,
+        arg0,
+        arg0,
+        arg0,
         arg0,
         arg0,
         arg0
--- AfxHookSource2/ReplaceName.h
+++ AfxHookSource2/ReplaceName.h
@@ -3,4 +3,4 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-bool HookReplaceName(HMODULE clientDll);
+void HookReplaceName(HMODULE clientDll);
