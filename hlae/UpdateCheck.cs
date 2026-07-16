b97636852b8eecae09285b5a386192bb285638eb
ChetdeJong <floweplay@gmail.com>
2026-07-16 16:33:14 +0200

    HLAE 2.191.1 (2026-07-16T14:23Z)

--- hlae/UpdateCheck.cs
+++ hlae/UpdateCheck.cs
@@ -51,11 +51,11 @@ namespace AfxGui
 
             m_Guids = new Guid[]{
                 // current GUID:
-                new Guid("04d26d84-f168-4bfe-b167-cba6cf551898"),
+                new Guid("ae30fb73-e288-445e-aa34-e353db2a8ae2"),
                 // current roll-back GUID:
-                new Guid("b8bf9afd-8f4f-4f80-a122-6ac26ad23850"),
+                new Guid("5ff2d03c-f1e1-49a0-b95d-1944364f8f1c"),
                 // old GUID(s) to accept:
-                new Guid("52dbdf8d-b0d1-49a0-867b-43bd9ada4f1f")
+                new Guid("04d26d84-f168-4bfe-b167-cba6cf551898")
             };
 
 	        m_Targets = new LinkedList<UpdateCheckNotificationTarget>();
